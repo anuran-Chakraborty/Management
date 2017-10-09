53b899cc02539209fecf74573deeee3d6fc2a488
unknown <ch.anuraan@gmail.com>
2017-10-09 19:53:57 +0530

    Tabbed Views

---
+++ app/src/main/java/com/example/anuran/management/TabPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.anuran.management;
+
+/**
+ * Created by ANURAN on 09-10-2017.
+ */
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TabPagerAdapter extends FragmentStatePagerAdapter {
+
+    int tabCount;
+
+    public TabPagerAdapter(FragmentManager fm, int numberOfTabs) {
+        super(fm);
+        this.tabCount = numberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                Company tab1 = new Company();
+                return tab1;
+            case 1:
+                Client tab2 = new Client();
+                return tab2;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}

4e94917935212e923c362c1b3e8e66ef0b976d59
Aditya Chowdhury <adityachowdhury21@gmail.com>
2017-10-12 22:20:41 +0530

    Marketting team form

--- app/src/main/java/com/example/anuran/management/Lead_Form.java
+++ app/src/main/java/com/example/anuran/management/Lead_Form.java
@@ -3,11 +3,11 @@ package com.example.anuran.management;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Lead_Form extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lead__form);
     }
 }

34d466316ff2057f5026597270f1b2c15a24c8c6
Aditya Chowdhury <adityachowdhury21@gmail.com>
2017-10-15 23:57:31 +0530

    Lead details

--- app/src/main/java/com/example/anuran/management/ViewClient.java
+++ app/src/main/java/com/example/anuran/management/ViewClient.java
@@ -47,11 +47,11 @@ public class ViewClient extends AppCompatActivity {
                     JSONObject obj=new JSONObject(response);
                     //Toast.makeText(getApplicationContext(),obj.getString("uname"), Toast.LENGTH_LONG).show();
                     TextView name=(TextView)findViewById(R.id.name);
-                    TextView uname=(TextView)findViewById(R.id.uname);
+                    //TextView uname=(TextView)findViewById(R.id.uname);
                     TextView mo1=(TextView)findViewById(R.id.mo1);
                     TextView ccid=(TextView)findViewById(R.id.ccid);
                     name.setText(obj.getString("name"));
-                    uname.setText(obj.getString("uname"));
+                    //uname.setText(obj.getString("uname"));
                     mo1.setText(obj.getString("mo1"));
                     ccid.setText(Integer.toString(100000+obj.getInt("id")));
 

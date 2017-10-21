f9dfcde383c9f1d15fba0b7167afd3589372ed2d
unknown <ch.anuraan@gmail.com>
2017-10-21 21:09:56 +0530

    Client

--- app/src/main/java/com/example/anuran/management/Constants.java
+++ app/src/main/java/com/example/anuran/management/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
         public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
         public static final String URL_LOGIN = ROOT_URL + "LoginUser.php";
         public static final String URL_LOGIN_FINANCE = ROOT_URL + "LoginFin.php";
+        public static final String URL_LOGIN_CLIENT = ROOT_URL + "LoginClient.php";
         public static final String URL_CONFIRM = "http://supernovasoftsys.com/Android/v1/ConfirmUser.php";
         public static final String URL_SEECLIENT = ROOT_URL + "SeeClient.php";
         public static final String URL_VIEWCLIENT = ROOT_URL + "ViewClient.php";
--- app/src/main/java/com/example/anuran/management/pocLogin.java
+++ app/src/main/java/com/example/anuran/management/pocLogin.java
@@ -1,13 +1,13 @@
 package com.example.anuran.management;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class Client extends AppCompatActivity {
+public class pocLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_client2);
+        setContentView(R.layout.activity_poc_login);
     }
 }

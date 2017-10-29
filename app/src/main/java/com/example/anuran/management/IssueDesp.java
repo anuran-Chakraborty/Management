492ab211ec5a90dddc4ad07375a4f40de1ac627f
unknown <ch.anuraan@gmail.com>
2017-10-29 22:08:33 +0530

    View issues

---
+++ app/src/main/java/com/example/anuran/management/IssueDesp.java
@@ -0,0 +1,11 @@
+package com.example.anuran.management;
+
+/**
+ * Created by ANURAN on 23-10-2017.
+ */
+
+public class IssueDesp {
+    public String issueid;
+    public String description;
+    public String client_id;
+}
--- app/src/main/java/com/example/anuran/management/Splash.java
+++ app/src/main/java/com/example/anuran/management/Splash.java
@@ -23,7 +23,7 @@ public class Splash extends AppCompatActivity {
             public void run()
             {
                 try {
-                    sleep(5000);
+                    sleep(2000);
                 }
                 catch (InterruptedException e)
                 {
